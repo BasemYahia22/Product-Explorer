38ba7218995d445b17006459d8bf332092fc4d28
BasemYahia22 <basemyahiagithup@gmail.com>
2026-06-14 16:36:30 +0300

    Add useDebouncedValue hook for optimized search input

---
+++ src/hooks/useDebouncedValue.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+export function useDebouncedValue(value, delay = 250) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => clearTimeout(timeoutId)
+  }, [value, delay])
+
+  return debouncedValue
+}
